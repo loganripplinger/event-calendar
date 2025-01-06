07aa3d9f9c6b16ca16d8f9df0d32a917306651c7
Logan <31484824+loganripplinger@users.noreply.github.com>
2025-01-05 18:21:36 -0800

    Add input.

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ import { Year } from "./Components/Year";
 import React from "react";
 import { DayPassed } from "./Components/DayPassed";
 
-const input = `
-Japan: January 1 to January 15
+const defaultInput = `Japan: January 1 to January 15
 DayLight bug free: March 4 to March 11
 one: Apr 1 to April 1
 two: Apr 15 to April 16
@@ -25,14 +24,21 @@ two lines: Feb 8 to Feb 23
 fourasdfasdfanda: January 31 to Feburary 3
 `;
 
+
 function App() {
+  const [showInput, setShowInput] = React.useState(false)
+  const [input, setInput] = React.useState(defaultInput)
+
   // Item 1 will always be the day, items 2+ are events. right now only dealing with one event. No overlaps.
   // 0: [{day}, {events}]
   const timeline = createTimelineWithDays();
 
-  const events = parseInput(input);
-  insertEventsIntoTimeline(timeline, events);
-
+  try {
+    const events = parseInput(input);
+    insertEventsIntoTimeline(timeline, events);
+  } catch (e) {
+    console.error(e)
+  }
   const emptySpacesInFront = 2;
 
   const emptiesDays = Array(emptySpacesInFront)
@@ -49,28 +55,32 @@ function App() {
   const daysPassed = Array(days).fill(null).map(() => <DayPassed />)
 
   return (
-    <div className="wrapper">
-      <div className="calendar">
-        {emptiesDays}
-        {timeline.map(([{ id, month, day }]) => (
-          <Day key={`${id} `} month={month} day={day} />
-        ))}
+    <>
+      <div className="wrapper">
+        <div className="calendar">
+          {emptiesDays}
+          {timeline.map(([{ id, month, day }]) => (
+            <Day key={`${id} `} month={month} day={day} />
+          ))}
+        </div>
+        <div className="calendar-events">
+          {emptiesEvents}
+          {timeline.map(([_, event]) =>
+            event ? (
+              <Event key={`${_.id} `} {...event} />
+            ) : (
+              <Empty key={`${_.id}`} hidden />
+            )
+          )}
+        </div>
+        <div className="calendar-days-passed">
+          {emptiesEvents}
+          {daysPassed}
+        </div>
       </div>
-      <div className="calendar-events">
-        {emptiesEvents}
-        {timeline.map(([_, event]) =>
-          event ? (
-            <Event key={`${_.id} `} {...event} />
-          ) : (
-            <Empty key={`${_.id}`} hidden />
-          )
-        )}
-      </div>
-      <div className="calendar-days-passed">
-        {emptiesEvents}
-        {daysPassed}
-      </div>
-    </div>
+      <button onClick={() => setShowInput(b => !b)}>Toggle input</button>
+      {showInput && <textarea className="input" value={input} onChange={e => setInput(e.target.value)} />}
+    </>
   );
 }
 
